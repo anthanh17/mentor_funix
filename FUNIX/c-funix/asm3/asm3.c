d6eb0294e83de5d90d6d5ea76caf54336474abe6
Annt <annguyenthe.work@gmail.com>
2023-08-16 00:16:26 +0700

    update asm3

--- FUNIX/c-funix/asm3/asm3.c
+++ FUNIX/c-funix/asm3/asm3.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAX_ROW 9
 #define MAX_COLUMN 9
@@ -22,7 +23,6 @@ point_t matrix[MAX_ROW][MAX_COLUMN];
 
 // ---------------------------------------------------------------------
 // Setup Queue
-
 typedef struct {
   point_t data[MAX];  // lưu trữ các nút
 
@@ -32,18 +32,16 @@ typedef struct {
 
 } Queue;
 
-// Hàm 1: Khởi tạo hàng đợi
+// Khởi tạo hàng đợi
 void initQueue(Queue *q) {
   q->front = 0;
   q->rear = -1;
 }
 
-// Hàm 2: Kiểm tra xem hàng đợi có rỗng hay không? Hàng đợi rỗng là hàng đợi mà
-// chỉ số của đầu của hàng đợi lớn hơn chỉ số cuối của hàng đợi
+// Kiểm tra xem hàng đợi có rỗng hay không?
 int isEmpty(Queue q) { return (q.rear < q.front); }
 
-// Hàm 3: Kiểm tra xem hàng đợi đã đầy chưa? Hàng đợi được xem là đầy là khi chỉ
-// số cuối của hàng đợi lớn hơn số lượng phần tử tối đa mà hàng đợi có thể chứa
+// Kiểm tra xem hàng đợi đã đầy chưa?
 int isFull(Queue q) {
   if ((q.rear - q.front) == MAX - 1) {
     return 1;
@@ -52,8 +50,8 @@ int isFull(Queue q) {
   }
 }
 
-// Hàm 4: Thêm phần tử vào cuối (rear) của hàng đợi.
-void enQueue(Queue *q, point_t x) {
+// Thêm phần tử vào cuối (rear) của hàng đợi.
+void enQueue(Queue *q, point_t *x) {
   if (!isFull(*q)) {
     if (q->rear == MAX - 1) {
       for (int i = q->front; i <= q->rear; i++) {
@@ -65,11 +63,11 @@ void enQueue(Queue *q, point_t x) {
     }
 
     q->rear = q->rear + 1;
-    q->data[q->rear] = x;
+    q->data[q->rear] = *x;
   }
 }
 
-//  Hàm 5: Lấy phần tử đầu (front) ra khỏi hàng đợi.
+// Lấy phần tử đầu (front) ra khỏi hàng đợi.
 point_t deQueue(Queue *q) {
   point_t d;
 
@@ -85,63 +83,135 @@ point_t deQueue(Queue *q) {
   return d;
 }
 
-// Hàm kiểm tra điểm bất kỳ có tọa độ nằm trong ma trận đã khai báo hay không
-bool checkCoordinate(int dong, int cot) {
-  bool ret = false;
-
-  if ((0 <= dong < MAX_ROW) && (0 <= cot < MAX_COLUMN)) {
-    ret = true;
-  }
-
-  return ret;
+/* -------------------------------------------------------------------------- */
+// Kiểm tra điểm bất kỳ có tọa độ nằm trong ma trận hay không?
+bool checkCoordinate(int x, int y) {
+  return (x >= 0 && x < MAX_ROW && y >= 0 && y < MAX_COLUMN);
 }
 
-// Tìm các điểm xung quanh của điểm được xét nằm trên đường đi từ điểm nguồn
-// O(0,0) đến điểm đích có giá trị bằng 1
-// void findSurroundingPoint(int dong, int cot, point_t surroundingPnt[4],
-//                           int *count) {
-//   // khởi tạo số biến đếm số lượng các nút có thể đi được quanh 1 vị trí
+// Tìm các điểm xung quanh của điểm được xét có giá trị bằng 1
+void findSurroundingPoint(int dong, int cot, point_t surroundingPnt[4],
+                          int *count) {
+  // khởi tạo số biến đếm số lượng các nút có thể đi được quanh 1 vị trí
 
-//   int tempCnt = 0;
+  int tempCnt = 0;
 
-//   // kiểm tra xem vị trí điểm bên phải có đến được không?
+  // kiểm tra xem vị trí điểm bên phải có đến được không?
 
-//   if ((checkCoordinate(dong, cot + 1) == true) &&
-//       (matrix[dong, cot + 1].value == 1)) {
-//     surroundingPnt[tempCnt] = matrix[dong, cot + 1];
+  if ((checkCoordinate(dong, cot + 1) == true) &&
+      (matrix[dong][cot + 1].value == 1)) {
+    surroundingPnt[tempCnt] = matrix[dong][cot + 1];
 
-//     tempCnt++;
-//   }
+    tempCnt++;
+  }
 
-//   // kiểm tra xem vị trí điểm bên dưới có đến được không?
+  // kiểm tra xem vị trí điểm bên dưới có đến được không?
 
-//   if ((checkCoordinate(dong + 1, cot) == true) &&
-//       (matrix[dong + 1, cot].value == 1)) {
-//     surroundingPnt[tempCnt] = matrix[dong + 1, cot];
+  if ((checkCoordinate(dong + 1, cot) == true) &&
+      (matrix[dong + 1][cot].value == 1)) {
+    surroundingPnt[tempCnt] = matrix[dong + 1][cot];
 
-//     tempCnt++;
-//   }
+    tempCnt++;
+  }
 
-//   // kiểm tra xem vị trí điểm bên trái có đến được không?
+  // kiểm tra xem vị trí điểm bên trái có đến được không?
 
-//   if ((checkCoordinate(dong, cot - 1) == true) &&
-//       (matrix[dong, cot - 1].value == 1)) {
-//     surroundingPnt[tempCnt] = matrix[dong, cot - 1];
+  if ((checkCoordinate(dong, cot - 1) == true) &&
+      (matrix[dong][cot - 1].value == 1)) {
+    surroundingPnt[tempCnt] = matrix[dong][cot - 1];
 
-//     tempCnt++;
-//   }
+    tempCnt++;
+  }
 
-//   // kiểm tra xem vị trí điểm bên trên có đến được không?
+  // kiểm tra xem vị trí điểm bên trên có đến được không?
 
-//   if ((checkCoordinate(dong - 1, cot) == true) &&
-//       (matrix[dong - 1, cot].value == 1)) {
-//     surroundingPnt[tempCnt] = matrix[dong - 1, cot];
+  if ((checkCoordinate(dong - 1, cot) == true) &&
+      (matrix[dong - 1][cot].value == 1)) {
+    surroundingPnt[tempCnt] = matrix[dong - 1][cot];
 
-//     tempCnt++;
-//   }
+    tempCnt++;
+  }
 
-//   *count = tempCnt;
-// }
+  *count = tempCnt;
+}
+
+// Định nghĩa cấu trúc của một nút trong linked list
+struct Node {
+  point_t data;
+  struct Node *next;
+};
+
+// Hàm để thêm một nút vào đầu danh sách liên kết
+void push(struct Node **head_ref, point_t new_data) {
+  struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+  new_node->data = new_data;
+  new_node->next = *head_ref;
+  *head_ref = new_node;
+}
+
+// Ham tim duong ngan nhat bang thuat toan BFS
+void shortestPathBFS(int destinationX, int destinationY) {
+  // Các hướng di chuyển: lên, xuống, trái, phải
+  int dx[4] = {-1, 1, 0, 0};
+  int dy[4] = {0, 0, -1, 1};
+
+  Queue q;
+  initQueue(&q);
+  enQueue(&q, &matrix[0][0]);
+  matrix[0][0].visited = true;
+
+  // Duyệt tất cả map theo chiều rộng
+  while (!isEmpty(q)) {
+    point_t temp = deQueue(&q);
+    point_t *current = &matrix[temp.row][temp.column];
+
+    // Kiểm tra nếu đã đến điểm đích
+    if (current->row == destinationX && current->column == destinationY) {
+      break;
+    }
+
+    // Xet cac huong di chuyen
+    for (int i = 0; i < 4; ++i) {
+      int newX = current->row + dx[i];
+      int newY = current->column + dy[i];
+
+      if (checkCoordinate(newX, newY) && !matrix[newX][newY].visited &&
+          matrix[newX][newY].value == 1) {
+        // thêm vào queue đển duyêt các phần tử xung quanh
+        enQueue(&q, &matrix[newX][newY]);
+        // đánh dấu điểm này là đã thăm
+        matrix[newX][newY].visited = true;
+        // đánh dấu điểm cha phụ vụ mục đích truy vết sau này
+        matrix[newX][newY].prev = current;
+      }
+    }  // end for
+  }    // end while
+
+  // Sau khi duyệt xong ta truy vết để tìm đường đi ngắn nhất
+  // Tồn tại 1 đường đi đến điểm đích
+  if (matrix[destinationX][destinationY].visited) {
+    point_t current = matrix[destinationX][destinationY];
+    struct Node *head = NULL;
+    int count = 0;
+
+    while (current.row != 0 || current.column != 0) {
+      push(&head, current);
+      current = *current.prev;
+      count++;
+    }
+    push(&head, matrix[0][0]);
+    count++;
+
+    printf("\nDuong di ngan nhat tu O(0,0) den A co do dai la %d o:\n", count);
+    while (head != NULL) {
+      printf(" -> (%d, %d)", head->data.row, head->data.column);
+      head = head->next;
+    }
+
+  } else {
+    printf("\nKhong co duong di tu diem (0,0) đến điểm đích \n");
+  }  // end if - else
+}
 
 int main() {
   int temp[MAX_COLUMN][MAX_ROW] = {
@@ -157,12 +227,12 @@ int main() {
       matrix[i][j].row = i;
       matrix[i][j].column = j;
       matrix[i][j].visited = false;
-      matrix[i][j].prev = NULL;
       matrix[i][j].value = temp[i][j];
+      matrix[i][j].prev = NULL;
     }
   }
 
-  printf("\nCHUONG TRINH TIM DUONG NGAN NHAT TU O(0,0) DEN A(dong, cot):");
+  printf("\nCHUONG TRINH TIM DUONG NGAN NHAT TU O(0,0) DEN A(dong, cot): \n");
   printf("\nMa tran 9x9 bieu dien cho mat phang 2 chieu: \n");
   for (i = 0; i < MAX_COLUMN; i++) {
     for (j = 0; j < MAX_ROW; j++) {
@@ -174,9 +244,7 @@ int main() {
 
   int dong = 6;
   int cot = 6;
-  printf("\nDuong di ngan nhat tu O(0,0) den A la: \n");
-
-  // TODO(annt, nhap matrix va tim duong ngan nhat)
+  shortestPathBFS(dong, cot);
 
   return 0;
 }
